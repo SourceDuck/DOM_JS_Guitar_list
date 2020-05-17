ff42fcac7123729fd71c820f6b7515866aee6e21
SourceDuck <63837674+SourceDuck@users.noreply.github.com>
2020-05-18 00:37:42 +0300

    Update guitars_script.js

--- guitars_script.js
+++ guitars_script.js
@@ -1,6 +1,7 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var li = document.querySelector("li");
 
 button.addEventListener("click", buttonClicked);
 input.addEventListener("keypress", enterPressed);
@@ -11,7 +12,7 @@ function enterPressed(event) {
 	}
 }
 
-/*For clarity and later use*/
+/*If needed for attributes*/
 function buttonClicked() {	
 		append();
 }
@@ -19,29 +20,22 @@ function buttonClicked() {
 function append() {	 
 	if (input.value.length > 0 && input.value.length < 20) {
 		var li = document.createElement("li");
-		li.appendChild(document.createTextNode(input.value));
+		li.appendChild(document.createTextNode(input.value + " "));
 		ul.appendChild(li);
 		input.value = "";
+				var btn = document.createElement("button");
+	     		btn.appendChild(document.createTextNode("Remove"));
+			li.appendChild(btn);
 	}
 }
 
+/*Delete any row, prefilled or new.*/
+	ul.addEventListener('click', (e) => {
+   if (e.target.matches('li')) {
+     e.target.classList.toggle("done");    
+   } 
 
-
-
-
-
-/*
-
-function addButton() {
-	var addButton = document.createElement("button");
-
-
-
-input.addEventListener("keypress", function(event) {
-	if (input.value.length > 0 && event.keyCode === 13) {
-		var li = document.createElement("li");
-		li.appendChild(document.createTextNode(input.value));
-		ul.appendChild(li);
-		input.value = "";
-	}
-})*/
+   if (e.target.matches('li button')) {
+     e.target.closest('li').remove();
+   }
+});
